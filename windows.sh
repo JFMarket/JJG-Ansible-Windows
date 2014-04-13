9c84c83312f217df8ee294ebe60803a6ee3cb92d
Ryan Robeson <ryan.robeson@gmail.com>
2014-04-13 15:10:43 -0400

    Switch to ubuntu based VMs

--- windows.sh
+++ windows.sh
@@ -13,6 +13,10 @@
 # Uncomment if behind a proxy server.
 # export {http,https,ftp}_proxy='http://username:password@proxy-host:80'
 
+# This script was forked from https://github.com/geerlingguy/JJG-Ansible-Windows
+# and is now setup to configure an Ubuntu based VM.
+# This version can be found at https://github.com/JFMarket/JJG-Ansible-Windows
+
 ANSIBLE_PLAYBOOK=$1
 ANSIBLE_HOSTS=$2
 TEMP_HOSTS="/tmp/ansible_hosts"
@@ -29,21 +33,14 @@ fi
 
 # Install Ansible and its dependencies if it's not installed already.
 if [ ! -f /usr/bin/ansible ]; then
-  echo "Installing Ansible dependencies and Git."
-  yum install -y git python python-devel
-  echo "Installing pip via easy_install."
-  wget http://peak.telecommunity.com/dist/ez_setup.py
-  python ez_setup.py && rm -f ez_setup.py
-  easy_install pip
-  # Make sure setuptools are installed crrectly.
-  pip install setuptools --no-use-wheel --upgrade
-  echo "Installing required python modules."
-  pip install paramiko pyyaml jinja2 markupsafe
-  echo "Installing Ansible."
-  pip install ansible
+  export DEBIAN_FRONTEND=noninteractive
+  echo "Installing Ansible from PPA"
+  sudo apt-add-repository ppa:rquillo/ansible
+  sudo apt-get update
+  sudo apt-get -q -y install ansible
 fi
 
 cp /vagrant/${ANSIBLE_HOSTS} ${TEMP_HOSTS} && chmod -x ${TEMP_HOSTS}
 echo "Running Ansible provisioner defined in Vagrantfile."
-ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --inventory-file=${TEMP_HOSTS} --extra-vars "is_windows=true" --connection=local
+ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --inventory-file=${TEMP_HOSTS} --extra-vars "is_windows=true" --connection=local --sudo
 rm ${TEMP_HOSTS}
